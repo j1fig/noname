b9ee39efad493dba90f10d984a164dbec2482af1
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2018-10-23 12:56:09 +0200

    time parsing now works up to spec

--- worker/times.go
+++ worker/times.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"encoding/csv"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,11 +12,11 @@ import (
 )
 
 type StopTime struct {
-	trip_id             int
-	arrival_time        time.Time
-	departure_time      time.Time
-	stop_id             int
-	stop_sequence       int
+	tripId             int
+	arrivalTime        time.Time
+	departureTime      time.Time
+	stopId             int
+	stopSequence       int
 	stop_headsign       int
 	pickup_type         int
 	drop_off_type       int
@@ -53,39 +54,50 @@ func ReadStopTimes(filename string) []StopTime {
 func parseStopTime(row []string) StopTime {
 	var stopTime StopTime
 
-	trip_id, err := strconv.Atoi(row[0])
+	tripId, err := strconv.Atoi(row[0])
 	if err != nil {
 		log.Fatalln("Error parsing trip_id: ", err, row)
 	}
 
-	// Date parsing code here!
-	// hourMinuteSecond := "15:24:06"
-	// arrival_time, err := time.Parse(hourMinuteSecond, row[1])
-	// if err != nil {
-	// 	log.Fatalln("Error parsing arrival_time: ", err, row)
-	// }
+	arrivalTime := parseTimeOfDay(row[1])
+	departureTime := parseTimeOfDay(row[2])
 
-	// departure_time, err := time.Parse(hourMinuteSecond, row[2])
-	// if err != nil {
-	// 	log.Fatalln("Error parsing departure_time: ", err, row)
-	// }
-
-	stop_id_tokens := strings.Split(row[3], "_")
-	stop_id, err := strconv.Atoi(stop_id_tokens[1])
+	stopIdTokens := strings.Split(row[3], "_")
+	stopId, err := strconv.Atoi(stopIdTokens[1])
 	if err != nil {
 		log.Fatalln("Error parsing stop_id: ", err, row)
 	}
 
-	stop_sequence, err := strconv.Atoi(row[4])
+	stopSequence, err := strconv.Atoi(row[4])
 	if err != nil {
 		log.Fatalln("Error parsing stop_sequence: ", err, row)
 	}
 
-	stopTime.trip_id = trip_id
-	// stopTime.arrival_time = arrival_time
-	// stopTime.departure_time = departure_time
-	stopTime.stop_id = stop_id
-	stopTime.stop_sequence = stop_sequence
+	stopTime.tripId = tripId
+	stopTime.arrivalTime = arrivalTime
+	stopTime.departureTime = departureTime
+	stopTime.stopId = stopId
+	stopTime.stopSequence = stopSequence
 
 	return stopTime
+}
+
+func parseTimeOfDay(t string) time.Time {
+	hourMinuteSecond := "15:04:05"
+	pt, err := time.Parse(hourMinuteSecond, t)
+	if err != nil {
+		// time can span over the 24h period if still on the same service day.
+		// see https://developers.google.com/transit/gtfs/reference/#stop_timestxt
+		nextDay := strings.Compare("23:59:59", t) < 0
+		// we can do this because we'll still know it's in the next day as the data
+		// pertains to the same trip_id and we have stop_sequence to enforce the order.
+		if nextDay {
+			var hour, minute, second int
+			fmt.Sscanf(t, "%d:%d:%d", &hour, &minute, &second)
+			hour -= 24
+			return time.Date(0, 0, 0, hour, minute, second, 0, time.UTC)
+		}
+	}
+
+	return pt
 }
