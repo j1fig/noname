3452dcd7faad182200d650a3e24bb13952e1b24c
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2018-10-24 13:07:44 +0200

    added more frontend stuff

--- static/src/index.js
+++ static/src/index.js
@@ -1,9 +1,16 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
 import _ from 'lodash';
 
 function component() {
   let element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpackerrz'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  $.ajax('api/stops');
+  setTimeout(function () {
+    console.log("hullo");
+  }, 5000);
 
   return element;
 }
